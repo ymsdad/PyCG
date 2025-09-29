2a4f6f09f138d038df015268b36bf4df43816e2c
ymsdad <18302010024@fudan.edu.cn>
2025-09-29 10:48:32 +0800

    add pos only args to default

--- pycg/processing/preprocessor.py
+++ pycg/processing/preprocessor.py
@@ -50,13 +50,18 @@ class PreProcessor(ProcessingBase):
 
     def _get_fun_defaults(self, node):
         defaults = {}
-        start = len(node.args.args) - len(node.args.defaults)
+        pos_args = []
+        if node.args.posonlyargs:
+            pos_args.extend(node.args.posonlyargs)
+        pos_args.extend(node.args.args)
+
+        start = len(pos_args) - len(node.args.defaults)
         for cnt, d in enumerate(node.args.defaults, start=start):
             if not d:
                 continue
 
             self.visit(d)
-            defaults[node.args.args[cnt].arg] = self.decode_node(d)
+            defaults[pos_args[cnt].arg] = self.decode_node(d)
 
         start = len(node.args.kwonlyargs) - len(node.args.kw_defaults)
         for cnt, d in enumerate(node.args.kw_defaults, start=start):
